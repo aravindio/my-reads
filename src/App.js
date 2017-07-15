11c4a91aaf5a178de23129cd0382cca2f8c98128
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-07-16 04:56:47 +0530

    Add SearchBooks component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -16,17 +17,7 @@ class BooksApp extends React.Component {
           <ListBooks />
         )}/>
         <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author"/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBooks />
         )}/>
       </div>
     )
