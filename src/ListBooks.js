b5667d1654a035e8027a1a90355ef50ef2d214b4
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-07-16 05:18:36 +0530

    Add search route Link

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 
 class ListBooks extends React.Component {
@@ -26,7 +27,7 @@ class ListBooks extends React.Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="search">Add a book</Link>
         </div>
       </div>
     )
