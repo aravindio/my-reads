2f18ebaa365fe7746fcd2578070acdd34bda2998
Aravind Raj Mahadevan <aravindrajmahadevan@gmail.com>
2017-07-16 05:04:14 +0530

    Get books from API

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
+    books: []
+  }
 
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
   }
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks books={this.state.books}/>
         )}/>
         <Route path="/search" render={() => (
           <SearchBooks />
